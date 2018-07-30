b43dbc869291eb935702ae453a19091d3d7065b2
Leandro Silva <leandrodoze@gmail.com>
2018-07-29 22:29:48 -0300

    cosmetics

--- src/cheesyd.c
+++ src/cheesyd.c
@@ -87,10 +87,10 @@ int main()
     for (int i = 0; i < 5; i++)
     {
         /*
-		* Create a global settings object used to store options that are not
-		* related to input objects, note that control of this object is parsed to
-		* the converter later, which is then responsible for freeing it
-		*/
+	 * Create a global settings object used to store options that are not
+	 * related to input objects, note that control of this object is parsed to
+	 * the converter later, which is then responsible for freeing it
+	 */
         gs = wkhtmltopdf_create_global_settings();
 
         /* We want the result to be storred in the file called test.pdf */
@@ -103,10 +103,10 @@ int main()
         //// wkhtmltopdf_set_global_setting(gs, "load.cookieJar", "myjar.jar");
 
         /*
-		* Create a input object settings object that is used to store settings
-		* related to a input object, note again that control of this object is parsed to
-		* the converter later, which is then responsible for freeing it
-		*/
+	 * Create a input object settings object that is used to store settings
+	 * related to a input object, note again that control of this object is parsed to
+	 * the converter later, which is then responsible for freeing it
+	 */
         os = wkhtmltopdf_create_object_settings();
 
         /* We want to convert to convert the qstring documentation page */
@@ -130,10 +130,10 @@ int main()
         wkhtmltopdf_set_warning_callback(c, warning);
 
         /*
-		* Add the the settings object describing the qstring documentation page
-		* to the list of pages to convert. Objects are converted in the order in which
-		* they are added
-		*/
+	 * Add the the settings object describing the qstring documentation page
+	 * to the list of pages to convert. Objects are converted in the order in which
+	 * they are added
+	 */
         wkhtmltopdf_add_object(c, os, NULL);
 
         /* Perform the actual convertion */
